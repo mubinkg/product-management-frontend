d4efbc616eb1a1db06a4cccc13812fb2c23e5fbc
mubin <mubin.ice.ru@gmail.com>
2024-07-05 21:49:01 +0600

    add container

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./globals.css";
+import { Container } from "reactstrap";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +18,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Container>
+          {children}
+        </Container>
+      </body>
     </html>
   );
 }
