293d298ae00b4a2ee9579d141df4f48aa525783e
mubin <mubin.ice.ru@gmail.com>
2024-07-07 23:27:13 +0600

    udpate footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,12 +3,15 @@ import { Container } from 'reactstrap'
 
 const Footer = () => {
     return (
-        <div style={{ backgroundColor: "#000419", minHeight: "586px"}}>
-            <div style={{marginTop: "20px" }}></div>
+        <div style={{ backgroundColor: "#000419" }}>
             <Container>
-                <div>
-                    <div style={{ backgroundColor: "#1C64F2", height: "441px", borderRadius: "40px"}}>
-
+                <div className='mt-4'>
+                    <div className='d-flex flex-column justify-content-center align-items-center' style={{ backgroundColor: "#1C64F2", height: "400px", borderRadius: "40px" }}>
+                        <h1 className='text-white'>Ready to get started?</h1>
+                        <span style={{ maxWidth: "600px", textAlign: "center", color: "white" }}>
+                            Lorem ipsum dolor sit amet consectetur. Lacus lacus nibh et nunc fringilla amet. Blandit in tortor varius orci vel quam quis sit. Viverra magna ullamcorper.
+                        </span>
+                        <button className='footer-btn mt-4'>Get Started</button>
                     </div>
                 </div>
             </Container>
